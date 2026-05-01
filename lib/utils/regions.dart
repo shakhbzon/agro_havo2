9c0845345814d617f406234a5b1fbb7321f1a296
shakhbzon <gojo15524@gmail.com>
2026-05-01 12:42:45 -0700

    Add Regions class with Uzbekistan regions dataAdd regions.dart with Uzbekistan regions and districts

---
+++ lib/utils/regions.dart
@@ -0,0 +1,17 @@
+class Regions {
+    static const Map<String, List<String>> uzbekistanRegions = {
+          "Toshkent": ["Toshkent shahri", "Angren", "Bekobod", "Olmaliq", "Chirchiq", "Yangiyo'l"],
+          "Andijon": ["Andijon shahri", "Asaka", "Xonobod", "Shahrixon", "Qorasuv"],
+          "Buxoro": ["Buxoro shahri", "Kogon", "G'ijduvon", "Vobkent"],
+          "Farg'ona": ["Farg'ona shahri", "Qo'qon", "Marg'ilon", "Quva", "Rishton"],
+          "Jizzax": ["Jizzax shahri", "Gallaorol", "Zomin", "Paxtakor"],
+          "Namangan": ["Namangan shahri", "Chust", "Kosonsoy", "Uychi"],
+          "Navoiy": ["Navoiy shahri", "Zarafshon", "Uchquduq", "Qiziltepa"],
+          "Qashqadaryo": ["Qarshi", "Shahrisabz", "Kitob", "Kasbi"],
+          "Samarqand": ["Samarqand shahri", "Kattaqo'rg'on", "Urgut", "Bulung'ur"],
+          "Sirdaryo": ["Guliston", "Shirin", "Yangiyer", "Boyovut"],
+          "Surxondaryo": ["Termiz", "Denov", "Sherobod", "Jarqo'rg'on"],
+          "Xorazm": ["Urganch", "Xiva", "Gurlan", "Shovot"],
+          "Qoraqalpog'iston": ["Nukus", "Xo'jayli", "Qo'ng'irot", "Mo'ynoq"],
+    };
+}
